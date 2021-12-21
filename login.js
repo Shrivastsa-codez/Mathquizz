889f4ffd9f1cb56dc9af67f9b5afa8776206530e
Shrivastsa-codez <78452805+Shrivastsa-codez@users.noreply.github.com>
2021-12-20 19:40:34 -0600

    Add files via upload

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+player1_name=player1_name_input;
+player2_name=player2_name_input;
 function adduser(){
     player1_name=document.getElementById("player1_name_input").value;
     player2_name=document.getElementById("player2_name_input").value;
